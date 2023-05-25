478cd1678f22d02a6918410edae7232aadcca82e
Skipper <ALPskipper@gmail.com>
2023-05-25 14:12:29 +0300

    Example005 Condition If Else

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maria")
+{
+Console.WriteLine("Wow it's Maria");
+}
+else
+{
+Console.Write("Hi, ");
+Console.WriteLine(username);
+}
