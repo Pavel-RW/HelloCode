7634e6c04828a5f53d7df53e08e83dc159f6fbe1
Skipper <ALPskipper@gmail.com>
2023-08-21 19:30:51 +0300

    add Ex013

---
+++ Ex013_FillMatrix/Program.cs
@@ -0,0 +1,62 @@
+﻿// Заполнение многомерных массивов - Матриц - FillMatrix
+
+// void PrintArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             Console.Write($"{matr[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// int[,] matrix = new int[3, 4];
+// PrintArray(matrix);
+// // Создание и печать Матрицы с помощью метода
+
+
+// void FillArray(int[,] matr)
+// {
+//  for (int i = 0; i < matr.GetLength(0); i++)
+//  {
+//  for (int j = 0; j < matr.GetLength(1); j++)
+//  {
+//  matr[i,j] = new Random().Next(1,10);//[1; 10)
+//  }
+//  }
+// }
+// FillArray(matrix);
+// Console.WriteLine();
+// PrintArray(matrix);
+// // Заполнение Матрицы Случайными/Рандомными числами с помощью метода
+
+
+// Полный код - Создание/Печать/Заполнение/Печать Массива с использованием методов
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);//[1; 10)
+        }
+    }
+}
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
