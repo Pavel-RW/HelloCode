58c6bdefafd283ff53774ddec35eb5da4f6858a5
Skipper <ALPskipper@gmail.com>
2023-08-25 21:03:59 +0300

    add Ex024

---
+++ Ex024_BypassDifferentStructures/Program.cs
@@ -0,0 +1,45 @@
+﻿// // Обход разных структур - Bypass different structures
+
+// ((4-2)*(1+3))/10
+
+//               /
+// ((4-2)*(1+3))   10
+
+//         /
+//       *   10
+// (4-2)         (1+3)
+
+//        /
+//      * 	 10
+//   -  	     +
+// 4	2      1   3
+
+
+
+string[] tree = { "emp", "/", "*", "10", "-", "+", "emp", "emp", "4", "2", "1", "3" };
+
+void InOrderTraversal(int pos = 1)
+{
+    if (pos < tree.Length && tree[pos] != "emp")
+    {
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+
+        if (left < tree.Length && tree[left] != "emp")
+        {
+            InOrderTraversal(left);
+        }
+
+        if (tree[pos] != "emp")
+        {
+            Console.Write(tree[pos] + " ");
+        }
+
+        if (right < tree.Length && tree[right] != "emp")
+        {
+            InOrderTraversal(right);
+        }
+    }
+}
+
+InOrderTraversal();
